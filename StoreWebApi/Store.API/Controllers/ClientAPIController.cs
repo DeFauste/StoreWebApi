78c3cf2011f3376f3a818b179719ec7e207ca751
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2025-01-13 20:47:11 +0300

    add: test controller

---
+++ StoreWebApi/Store.API/Controllers/ClientAPIController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace Store.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/client")]
+    public class ClientAPIController : ControllerBase
+    {
+        [HttpGet("get")]
+        public IEnumerable<int> GetInts() => new int[] { 1, 2, 3 };
+    }
+}
